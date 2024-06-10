471c844dfe64a436af40872a7d9b47d1e8d45273
Kyle Freidhof <freidhofkyle@proton.me>
2024-06-10 03:33:40 -0400

    Turned my audio convert script in to an actual usable script

--- audio_converter/avert.sh
+++ audio_converter/avert.sh
@@ -221,10 +221,94 @@ convert_mp3_to_aac() {
 
 }
 
-# ffmpeg -i *.flac *.mp3
+help_guide() {
+	printf "-a or -A will convert a flac to an mp3 \n"
+	printf "-b or -B will convert a mp3 to an opus \n"
+	printf "-c or -C will convert a opus to an mp3 \n"
+	printf "-d or -D will convert aac to an opus \n"
+	printf "-e or -E will convert mp3 to an ogg \n"
+	printf "-f or -F will convert aac to an ogg \n"
+	printf "-g or -G will convert flac to an aac \n"
+	printf "-h or -H this will display the help guide and give a list of options you can run\n"
+	printf "-i or -I will convert flac to an ogg \n"
+	printf "-j or -J will convert aac to mp3 \n"
+	printf "-k or -K will convert mp3 to acc and you get to choose the container you use \n"
+	printf "-l or -L will convert flac to opus \n"
+
+}
 
 
-# for file in *.flac; do
- #      f="${file%.*}"
-  #     ffmpeg -i "$file" -b:a 320k "$f.mp3"       
-# done
+while getopts "abcdefghijklmnopqrstuvwxyz" opt; do  
+
+case ${opt} in 
+	a|A)
+		convert_flac_to_mp3
+		return 0
+		;;
+
+	b|B)
+		convert_mp3_to_opus
+		return 0
+		;;
+
+	c|C)
+		convert_opus_to_mp3
+		return 0
+		;;
+
+	d|D)
+		convert_aac_to_opus
+		return 0
+		;;
+
+	e|E)
+		convert_mp3_to_ogg
+		return 0
+		;;
+
+	f|F)
+		convert_aac_to_ogg
+		return 0
+		;;
+
+	g|G)
+		convert_flac_to_aac
+		return 0
+		;;
+
+	h|H)
+		help_guide
+		return 0
+		;;
+
+	i|I)
+		convert_flac_to_ogg
+		return 0
+		;; 
+
+	j|J)
+		convert_aac_to_mp3
+		return 0
+		;;
+
+	k|K)
+		convert_mp3_to_aac
+		return 0
+		;;
+
+	l|L)
+		convert_flac_to_opus
+		return 0
+		;;
+
+	*)
+		echo "Error: Option not avalible or format not avalible"
+		echo "More formats are coming soon"
+		return 1
+
+
+
+
+esac
+
+done
